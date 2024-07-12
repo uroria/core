d98f85c8363ce8a4c4e2b936f89e70657df0a265
julian-siebert <mail@julian-siebert.de>
2024-07-12 10:44:45 +0200

    Maven Central deploy

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,13 @@
 import com.github.jengelman.gradle.plugins.shadow.ShadowPlugin
 import org.apache.tools.ant.filters.ReplaceTokens
+import java.time.Duration
 
 plugins {
     `java-library`
     `maven-publish`
     alias(libs.plugins.shadow)
+    signing
+    alias(libs.plugins.nexusPublish)
 }
 
 version = System.getenv("CORE_VERSION") ?: "dev"
@@ -91,5 +94,88 @@ subprojects {
 sourceSets {
     main {
         java.srcDir(file("src/main/java"))
+    }
+}
+
+tasks {
+    nexusPublishing {
+        useStaging.set(true)
+        this.packageGroup.set("com.uroria")
+
+        transitionCheckOptions {
+            maxRetries.set(360) // 1h
+            delayBetween.set(Duration.ofSeconds(10))
+        }
+
+        repositories.sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+
+            val sonatypeUsername = System.getenv("SONATYPE_USERNAME")
+            val sonatypePassword = System.getenv("SONATYPE_PASSWORD")
+
+            if (sonatypeUsername != null) {
+                username.set(sonatypeUsername)
+                password.set(sonatypePassword)
+            }
+        }
+    }
+
+    publishing.publications.create<MavenPublication>("maven") {
+        groupId = "com.uroria"
+        artifactId = "core"
+        version = project.version.toString()
+
+        from(project.components["java"])
+
+        pom {
+            name.set(this@create.artifactId)
+            description.set(projectDescription)
+            url.set("https://github.com/uroria/core")
+
+            licenses {
+                license {
+                    name.set("Apache 2.0")
+                    url.set("https://github.com/uroria/core/blob/main/LICENSE")
+                }
+            }
+
+            developers {
+                developer {
+                    id.set("julian-siebert")
+                    name.set("Julian Siebert")
+                    organization.set("Uroria")
+                    organizationUrl.set("https://github.com/uroria")
+                    email.set("mail@julian-siebert.de")
+                }
+            }
+
+            issueManagement {
+                system.set("GitHub")
+                url.set("https://github.com/uroria/core/issues")
+            }
+
+            scm {
+                connection.set("scm:git:git://github.com/uroria/core.git")
+                developerConnection.set("scm:git:git@github.com:uroria/uroria.git")
+                url.set("https://github.com/uroria/core")
+                tag.set("HEAD")
+            }
+
+            ciManagement {
+                system.set("GitHub Actions")
+                url.set("https://github.com/uroria/core/actions")
+            }
+        }
+    }
+
+    signing {
+        isRequired = System.getenv("CI") != null
+
+        val privateKey = System.getenv("GPG_PRIVATE_KEY")
+        val keyPassphrase = System.getenv("GPG_PASSPHRASE")
+        useInMemoryPgpKeys(privateKey, keyPassphrase)
+
+        sign(publishing.publications)
     }
 }
